34fe9c7969fe6778015fdc2673a98b49f79a459e
Rahul Kanianchalil <rkania3@illinois.edu>
2019-05-09 20:49:56 -0500

    adding indicator function;

--- common/alpha.py
+++ common/alpha.py
@@ -6,7 +6,7 @@ from common.config import config
 
 class Alpha(object):
 
-    # Def: Get intraday data from alpha vantage
+    # Def: Get stock data from alpha vantage
     # In: freq - time frame, symbol - ticker symbol, interval = 1min, 5min, 15min, 30min, or 60min, data_type - json/csv
     # Out: file location of data
     def get_data(self, freq, symbol, interval, data_type='csv'):
@@ -14,9 +14,9 @@ class Alpha(object):
         key = config.get('alpha-vantage', 'api.key')
 
         # Format URL
-        url = config.get('alpha-vantage', 'intraday.data')
+        url = config.get('alpha-vantage', 'stock.data')
 
-        url = url.format(freq, symbol, interval, key, data_type)
+        url = url.format(freq, symbol, interval, data_type, key)
 
         print("Getting data from: {}".format(url))
 
@@ -54,3 +54,37 @@ class Alpha(object):
             TIME_SERIES_MONTHLY
             TIME_SERIES_MONTHLY_ADJUSTED
         """
+
+    # Get indicator data from Alpha Vantage
+    # In: function - indicator, symbol - stock symbol, interval - time between 2 data points
+    #   time_period - number of points for calc, series_type - desired price type, data_type - csv or json
+    def get_indicator(self, function, symbol, interval, time_period, series_type='close', data_type='csv'):
+        # Get AlphaVantage API Key
+        key = config.get('alpha-vantage', 'api.key')
+
+        # Format URL
+        url = config.get('alpha-vantage', 'indicator.data')
+
+        url = url.format(function, symbol, interval, time_period, series_type, data_type, key)
+
+        print("Getting data from: {}".format(url))
+
+        # Grab Data
+        try:
+            response = requests.get(url)
+
+        except Exception as e:
+            raise e
+
+        # Save data
+        workspace = os.getcwd()
+        time = datetime.now().date()
+
+        data_loc = '{}/data/'.format(workspace)
+
+        file_name = '{}_{}_{}.{}'.format(function, symbol, time, data_type)
+
+        with open(data_loc + file_name, 'w+') as file:
+            file.write(response.content.decode("utf-8") )
+
+        return data_loc + file_name
